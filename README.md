620e5032bd87348fcfaac3052a9df55663e8ad99
jaywcjlove <398188662@qq.com>
2024-07-29 01:31:29 +0800

    init project.

---
+++ README.md
@@ -0,0 +1,41 @@
+Release Upload Assets
+===
+
+[![Buy me a coffee](https://img.shields.io/badge/Buy%20me%20a%20coffee-048754?logo=buymeacoffee)](https://jaywcjlove.github.io/#/sponsor)
+[![CI](https://github.com/jaywcjlove/github-action-upload-assets/actions/workflows/ci.yml/badge.svg)](https://github.com/jaywcjlove/github-action-upload-assets/actions/workflows/ci.yml)
+
+GitHub Action to upload multiple assets to a release
+
+## Example Usage
+
+```yml
+- name: Release Upload Assets
+  uses: jaywcjlove/github-action-upload-assets@main
+  if: steps.create_tag.outputs.successful == 'true'
+  with:
+    asset-path: '["./target/release/sgo-*"]'
+```
+
+## Inputs
+
+- `asset-path` The paths to the assets you want to upload as a JSON array. You can use a [glob](https://www.npmjs.com/package/glob) pattern.
+- `tag` Specify the release tag name, for example: `v1.23.12`
+
+## Outputs
+
+- `browser_download_urls` The URL users can navigate to in order to download the uploaded asset
+
+## See Also
+
+- [Github Release Changelog Generator](https://github.com/jaywcjlove/changelog-generator) A GitHub Action that compares the commit differences between two branches
+- [Create Tags From](https://github.com/jaywcjlove/create-tag-action) Auto create tags from commit or package.json.
+- [Github Action Contributors](https://github.com/jaywcjlove/github-action-contributors) Github action generates dynamic image URL for contributor list to display it!
+- [Generated Badges](https://github.com/jaywcjlove/generated-badges) Create a badge using GitHub Actions and GitHub Workflow CPU time (no 3rd parties servers)
+- [Create Coverage Badges](https://github.com/jaywcjlove/coverage-badges-cli) Create coverage badges from coverage reports. (no 3rd parties servers)
+- [Github Action package](https://github.com/jaywcjlove/github-action-package) Read and modify the contents of `package.json`.
+- [Github Action EJS](https://github.com/jaywcjlove/github-action-package) A github action to render a ejs template using github context.
+- [Modify File Content](https://github.com/jaywcjlove/github-action-modify-file-content) Replace text content and submit content.
+
+## License
+
+Licensed under the MIT License.
---
+++ src/index.ts
@@ -0,0 +1,95 @@
+import { context, getOctokit } from '@actions/github';
+import { getInput, setOutput, setFailed } from '@actions/core';
+import { glob } from 'glob';
+import fs from 'fs';
+import path from 'path';
+
+/**
+ * get the release URL from the tag
+ * @param tag 'v1.12.23'
+ * @returns 
+ */
+async function getReleaseURL(tag: string | undefined) {
+  const { owner, repo } = context.repo;
+  // Get the tag name from the triggered action
+  const tagName = context.ref;
+  const myToken = getInput('token')
+
+  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.12.23' to 'v1.12.23'
+  const currentTag = tag || tagName.replace("refs/tags/", "");
+  const octokit = getOctokit(myToken);
+  
+  // Get a release from the tag name
+  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+  const getReleaseResponse = await octokit.rest.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag: currentTag
+  });
+  return getReleaseResponse.data;
+}
+
+function getAssetName(assetPath: string): string | string[] | undefined {
+  try {
+    return JSON.parse(assetPath) as string | string[];
+  } catch (error) {
+    if (assetPath) {
+      return assetPath
+    }
+  }
+  return undefined;
+}
+
+;(async () => {
+  /**
+   * Get the path to the file to upload
+   */
+  const assetPath = getAssetName(getInput('asset-path', { required: true }));
+  const tagName: string | undefined = getInput('tag') || undefined;
+  const myToken = getInput('token')
+  try {
+    if (!assetPath || assetPath.length === 0) {
+      throw new Error('asset-path must contain a JSON array of quoted paths');
+    }
+    const files = await glob(assetPath, {})
+    if (files.length === 0) {
+      throw new Error('No files found');
+    }
+    const octokit = getOctokit(myToken);
+    const release = await getReleaseURL(tagName)
+    const downloadURLs = []
+    for(let i = 0; i < files.length; i++) {
+      const assetFile = files[i];
+      // Determine content-length for header to upload asset
+      const contentLength = (filePath: string) => fs.statSync(filePath).size;
+      const contentType = 'application/octet-stream';
+      //const contentType = "binary/octet-stream"
+      // Setup headers for API call, see Octokit Documentation: https://octokit.github.io/rest.js/v20#repos-update-release-asset for more information
+      const headers = { 
+        'content-type': contentType, 
+        'content-length': contentLength(assetFile)
+      };
+      const assetName = path.basename(assetFile);
+      const data = fs.readFileSync(assetFile);
+      // Upload a release asset
+      // API Documentation: https://docs.github.com/en/rest/releases/releases?apiVersion=2022-11-28#update-a-release
+      // Octokit Documentation: https://octokit.github.io/rest.js/v20#repos-update-release-asset
+      const response = await octokit.rest.repos.uploadReleaseAsset({
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        release_id: release.id,
+        headers,
+        name: assetName,
+        data: data.toString()
+      });
+      downloadURLs.push(response.data.browser_download_url)
+      if (response.status < 200 || response.status > 299) {
+        new Error(`Asset upload failed "${assetPath}. Response:" ${response}`)
+      }
+    }
+    setOutput('browser_download_urls', JSON.stringify(downloadURLs));
+  } catch (error) {
+    setFailed(error as Error);
+  }
+})();
