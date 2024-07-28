34dde0d9283a63b8d0b54da0b7df4257985a8d58
jaywcjlove <398188662@qq.com>
2024-07-29 02:16:37 +0800

    feat: using a new upload request.

--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,7 @@
-import { context, getOctokit } from '@actions/github';
+import { context } from '@actions/github';
 import { getInput, setOutput, setFailed, info } from '@actions/core';
 import { glob } from 'glob';
-import fs from 'fs';
-import path from 'path';
-
-/**
- * get the release URL from the tag
- * @param tag 'v1.12.23'
- * @returns 
- */
-async function getReleaseURL(tag: string | undefined) {
-  const { owner, repo } = context.repo;
-  // Get the tag name from the triggered action
-  const tagName = context.ref;
-  const myToken = getInput('token')
-
-  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.12.23' to 'v1.12.23'
-  const currentTag = tag || tagName.replace("refs/tags/", "");
-  const octokit = getOctokit(myToken);
-  info(`Getting release for tag: ${currentTag}`);
-  // Get a release from the tag name
-  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
-  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
-  const getReleaseResponse = await octokit.rest.repos.getReleaseByTag({
-    owner,
-    repo,
-    tag: currentTag
-  });
-  return getReleaseResponse.data;
-}
-
-function getAssetName(assetPath: string): string | string[] | undefined {
-  try {
-    return JSON.parse(assetPath) as string | string[];
-  } catch (error) {
-    if (assetPath) {
-      return assetPath
-    }
-  }
-  return undefined;
-}
-
-// Function to upload the file
-async function uploadFile(owner: string, repo: string, releaseId: number, filePath: string) {
-  const myToken = getInput('token')
-  const octokit = getOctokit(myToken);
-
-  const fileContent = fs.readFileSync(filePath); // Read file content as Buffer
-  const fileStat = fs.statSync(filePath);
-  const fileName = path.basename(filePath);
-
-  const headers = { 
-    'content-type': 'application/octet-stream',
-    'content-length': fileStat.size
-  };
-
-  const response = await octokit.rest.repos.uploadReleaseAsset({
-    owner: owner,
-    repo: repo,
-    release_id: releaseId,
-    headers,
-    name: fileName,
-    data: fileContent.toString() // Use file content directly
-  });
-
-  return response;
-}
+import { getAssetName, getReleaseURL, uploadFile, requestUploadFile } from './utils';
 
 ;(async () => {
   /**
@@ -83,17 +19,21 @@ async function uploadFile(owner: string, repo: string, releaseId: number, filePa
       throw new Error('No files found');
     }
     const release = await getReleaseURL(tagName)
+//    release.upload_url
     const downloadURLs = []
     for(let i = 0; i < files.length; i++) {
       const assetFile = files[i];
       info(`Uploading asset(${context.repo.owner}/${context.repo.repo}): ${assetFile}`);
-      const response = await uploadFile(context.repo.owner, context.repo.repo, release.id, assetFile);
-      downloadURLs.push(response.data.browser_download_url)
-      if (response.status < 200 || response.status > 299) {
-        new Error(`Asset upload failed "${assetPath}. Response:" ${response}`)
-      }
+
+      const requestUploadResponse = await requestUploadFile(release.upload_url, assetFile);
+      info(`requestUploadResponse: ${JSON.stringify(requestUploadResponse)}`);
+      // const response = await uploadFile(context.repo.owner, context.repo.repo, release.id, assetFile);
+      // downloadURLs.push(response.data.browser_download_url)
+      // if (response.status < 200 || response.status > 299) {
+      //   new Error(`Asset upload failed "${assetPath}. Response:" ${response}`)
+      // }
     }
-    setOutput('browser_download_urls', JSON.stringify(downloadURLs));
+    // setOutput('browser_download_urls', JSON.stringify(downloadURLs));
   } catch (error) {
     setFailed(error as Error);
   }
---
+++ src/utils.ts
@@ -0,0 +1,94 @@
+import fs from 'fs';
+import path from 'path';
+import { context, getOctokit } from '@actions/github';
+import { getInput, info } from '@actions/core';
+import FormData from 'form-data';
+
+/**
+ * get the release URL from the tag
+ * @param tag 'v1.12.23'
+ * @returns 
+ */
+export async function getReleaseURL(tag: string | undefined) {
+  const { owner, repo } = context.repo;
+  // Get the tag name from the triggered action
+  const tagName = context.ref;
+  const myToken = getInput('token')
+
+  // This removes the 'refs/tags' portion of the string, i.e. from 'refs/tags/v1.12.23' to 'v1.12.23'
+  const currentTag = tag || tagName.replace("refs/tags/", "");
+  const octokit = getOctokit(myToken);
+  info(`Getting release for tag: ${currentTag}`);
+  // Get a release from the tag name
+  // API Documentation: https://developer.github.com/v3/repos/releases/#create-a-release
+  // Octokit Documentation: https://octokit.github.io/rest.js/#octokit-routes-repos-create-release
+  const getReleaseResponse = await octokit.rest.repos.getReleaseByTag({
+    owner,
+    repo,
+    tag: currentTag
+  });
+  return getReleaseResponse.data;
+}
+
+export function getAssetName(assetPath: string): string | string[] | undefined {
+  try {
+    return JSON.parse(assetPath) as string | string[];
+  } catch (error) {
+    if (assetPath) {
+      return assetPath
+    }
+  }
+  return undefined;
+}
+
+// Function to upload the file
+export async function uploadFile(owner: string, repo: string, releaseId: number, filePath: string) {
+  const myToken = getInput('token')
+  const octokit = getOctokit(myToken);
+
+  const fileContent = fs.readFileSync(filePath); // Read file content as Buffer
+  const fileStat = fs.statSync(filePath);
+  const fileName = path.basename(filePath);
+
+  const headers = { 
+    'content-type': 'application/octet-stream',
+    'content-length': fileStat.size
+  };
+
+  const response = await octokit.rest.repos.uploadReleaseAsset({
+    owner: owner,
+    repo: repo,
+    release_id: releaseId,
+    headers,
+    name: fileName,
+    data: fileContent.toString() // Use file content directly
+  });
+
+  return response;
+}
+
+// Function to upload the file
+export async function requestUploadFile(upload_url: string, filePath: string) {
+  const myToken = getInput('token')
+  const octokit = getOctokit(myToken);
+  const fileStream = fs.createReadStream(filePath);
+  const fileName = path.basename(filePath);
+  const form = new FormData();
+  form.append('file', fileStream, {
+    filename: fileName,
+    contentType: 'application/octet-stream'
+  });
+
+  const headers = { ...form.getHeaders(), 'Authorization': `token ${myToken}` };
+  const response = await octokit.request({
+    method: 'POST',
+    url: upload_url,
+    headers: headers,
+    data: form,
+    params: {
+      name: fileName
+    }
+  });
+
+  return response;
+}
